600685c73e78021300a53bb687d69b75c5db73e3
jychan99 <108382087+jychan99@users.noreply.github.com>
2022-09-08 21:32:04 +0900

    how to use backtick

--- js/text.js
+++ js/text.js
@@ -3,7 +3,9 @@ let month = today.getMonth() + 1; // 월
 let date = today.getDate(); // 날짜
 
 const next = document.querySelector(".leaf");
-const back = document.querySelector(".back");
+const back = document.querySelector(".fa-chevron-left");
 
 next.addEventListener("click", moveTo3);
 back.addEventListener("click", moveTo2);
+
+document.querySelector(".text").innerText = `dsadsa`;

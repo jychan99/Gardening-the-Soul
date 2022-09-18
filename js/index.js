6c5fd68ca13da82338d3c868e860ab530ed6a92c
jychan99 <108382087+jychan99@users.noreply.github.com>
2022-09-18 13:30:52 +0900

    update closing-text animation

--- js/index.js
+++ js/index.js
@@ -16,6 +16,7 @@ const next = document.querySelector(".leaf");
 const back = document.querySelector(".fa-chevron-left");
 const Ani_date = document.querySelector(".section1__date");
 const Ani_text = document.querySelector(".section1__text");
+const closing_text = document.querySelector(".closing__text");
 
 function deleteHidden() {
   homepageLeaf.classList.remove("hidden");
@@ -34,11 +35,19 @@ function animationstart() {
   Ani_text.classList.remove("paused");
   Ani_date.classList.add("play");
   Ani_text.classList.add("play");
+  next.classList.remove("paused");
+  next.classList.add("play");
+}
+function closing_animationstart() {
+  closing_text.classList.remove("hidden");
+  closing_text.classList.remove("paused");
+  closing_text.classList.add("play");
 }
 
 homepageLeaf.addEventListener("mouseover", deleteHidden);
-next.addEventListener("mouseover", deleteHidden_next);
 homepageLeaf.addEventListener("click", moveTo2);
 homepageLeaf.addEventListener("click", animationstart);
+next.addEventListener("mouseover", deleteHidden_next);
 next.addEventListener("click", moveTo3);
+next.addEventListener("click", closing_animationstart);
 back.addEventListener("click", moveTo2);
